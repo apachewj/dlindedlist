496714649d018d6eff103273e72c41928b773343
lixiaodong <35631249@qq.com>
2018-04-01 00:07:30 +0800

    基本方法

--- src/main/java/com/structure/DoubleLinkedList.java
+++ src/main/java/com/structure/DoubleLinkedList.java
@@ -21,25 +21,47 @@ public class DoubleLinkedList<T> {
     }
 
     /**
-     *
+     * 双链表往前插入一个元素
      * @param e
-     * @param prev
-     * @param next
      * @return
      */
-    public DoubleLinkedList<T> prepend(T e, DNode<T> prev, DNode<T> next) {
+    public DoubleLinkedList<T> prepend(T e) {
 
+        if (this.first == null) {
+            this.first = new DNode<T>(e, null, null);
+            this.last = this.first;
+        } else {
+            DNode<T> first_old = this.first;
+            DNode<T> first_new = new DNode<T>(e, null, first_old);
+            first_old.setPrev(first_new);
+            this.first = first_new;
+        }
         return this;
     }
 
     /**
-     *
+     * 双链表往后追加一个元素
      * @param e
-     * @param prev
-     * @param next
      * @return
      */
-    public DoubleLinkedList<T> append(T e, DNode<T> prev, DNode<T> next) {
+    public DoubleLinkedList<T> append(T e) {
+        if (this.last == null) {
+            this.last = new DNode<T>(e, null, null);
+            this.first = this.last;
+        } else {
+            DNode<T> last_old = this.last;
+            DNode<T> last_new = new DNode<T>(e, last_old, null);
+            last_old.setNext(last_new);
+            this.last = last_new;
+        }
+        return this;
+    }
+
+    /**
+     * 双链表转置
+     * @return
+     */
+    public DoubleLinkedList<T> reverse() {
 
         return this;
     }
